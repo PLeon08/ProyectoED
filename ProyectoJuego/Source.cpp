d9a1016d0e4475c6a561a11c8e207760dcbf543b
Pablo León <pjleon.tec@gmail.com>
2020-10-16 23:27:19 -0600

    Vidas listas, enemigo con animacion pero sin movimiento, pantalla de derrota y condicion de derrota lista, falta editar la pantalla.

--- ProyectoJuego/Source.cpp
+++ ProyectoJuego/Source.cpp
@@ -5,21 +5,26 @@
 #include <SDL_ttf.h>
 #include <sstream>
 #include <windows.h>
+#include <SDL_mixer.h>
 
 using namespace std;
 
 /*Declaración*/
 const Uint8* estadoteclado;
-int posx, posy;
+int posx, posy, posxE, posyE;
 bool quit;
 SDL_Event event;
 SDL_Window* window;
+SDL_Window* ventanaDerrota;
 SDL_Renderer* renderer;
 SDL_Surface* image;
-SDL_Texture* texture;
+SDL_Texture* texturapersonaje;
 
 SDL_Surface* fondo;
 SDL_Texture* texturafondo;
+
+SDL_Surface* enemigo;
+SDL_Texture* texturaenemigo;
 
 SDL_Rect rectFuente;
 SDL_Rect rectDestino;
@@ -27,23 +32,19 @@ SDL_Rect rectPanel;
 
 SDL_bool Colision;
 
+Mix_Chunk* efectocofre;
+
 
 bool cofreAbierto = false;
 bool llaveObtenida = false;
 
 int monedas;
 int puntos;
-int vida = 3;
+int vidas;
 const char* Texto;
 int cuenta_atras;
 
 /*Fin Declaración*/
-
-
-
-
-
-
 
 /*
   1= muro vertical
@@ -202,7 +203,8 @@ void pintarmapa(SDL_Rect personaje) {
                 break;
             case 17://cofre                                                                       
                 Colision = SDL_HasIntersection(&personaje, &rectDestino);
-                if (Colision && llaveObtenida == true) {
+                if (Colision && llaveObtenida == true && cofreAbierto==false) {
+                    Mix_PlayChannel(-1, efectocofre, 0);
                     cofreAbierto = true;
                     SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectDestino);
                     rectFuente = { 192, 3456, 32, 32 };
@@ -230,7 +232,7 @@ void pintarmapa(SDL_Rect personaje) {
                     SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectDestino);
                    // mapa[i][j] = 0;
                    // Sonido = 2;
-                    vida--;
+                    vidas--;
                 }
                 else {
 
@@ -273,10 +275,6 @@ void MostrarTexto(const char* Texto, SDL_Rect PosicionTexto, int tamano) {
     SDL_RenderCopy(renderer, Textura, NULL, &PosicionTexto);
 }
 
-
-
-
-
 /*
 
 void cronometro() {
@@ -294,7 +292,6 @@ void cronometro() {
 }
 */
 
-
 void panel() {
     stringstream TextoTex;
     rectFuente = { 64, 3963, 32, 32 };//moneda
@@ -303,16 +300,8 @@ void panel() {
     TextoTex << ": " << monedas;
     MostrarTexto(TextoTex.str().c_str(), { 732, 160, 32, 32 }, 10);
     ////////////////////////////////////
-    rectFuente = { 160, 3904, 32, 32 };//CAMBIAR POR CORAZON...ESTO ES UN EJEMPLO
-    rectPanel = { 652,64,32,32 };
-    SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
-    rectPanel = { 700,64,32,32 };
-    SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
-    rectPanel = { 748,64,32,32 };
-    SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
+    
 }
-
-
 
 bool existeColisionArriba(int posxPersonaje, int posyPersonaje) {
     int posyMuro;
@@ -322,7 +311,7 @@ bool existeColisionArriba(int posxPersonaje, int posyPersonaje) {
     {
         for (int columna = 0; columna < 20; columna++) {
             if (mapa[fila][columna] == 1 || mapa[fila][columna] == 2 || mapa[fila][columna] == 3 || mapa[fila][columna] == 4 || mapa[fila][columna] == 5 || mapa[fila][columna] == 6 || mapa[fila][columna] == 7 || mapa[fila][columna] == 8
-                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 15 || mapa[fila][columna] == 15) {
+                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 14 || mapa[fila][columna] == 15) {
                 posxMuro = columna * 32;
                 posyMuro = (fila * 32) + 32;
                 if (posxMuro == posxPersonaje && posyMuro == posyPersonaje) {
@@ -342,7 +331,7 @@ bool existeColisionAbajo(int posxPersonaje, int posyPersonaje) {
     {
         for (int columna = 0; columna < 20; columna++) {
             if (mapa[fila][columna] == 1 || mapa[fila][columna] == 2 || mapa[fila][columna] == 3 || mapa[fila][columna] == 4 || mapa[fila][columna] == 5 || mapa[fila][columna] == 6 || mapa[fila][columna] == 7 || mapa[fila][columna] == 8
-                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 15 || mapa[fila][columna] == 15) {
+                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 14 || mapa[fila][columna] == 15) {
                 posxMuro = columna * 32;
                 posyMuro = (fila * 32) - 32;
                 if (posxMuro == posxPersonaje && posyMuro == posyPersonaje) {
@@ -362,7 +351,7 @@ bool existeColisionIzquierda(int posxPersonaje, int posyPersonaje) {
     {
         for (int columna = 0; columna < 20; columna++) {
             if (mapa[fila][columna] == 1 || mapa[fila][columna] == 2 || mapa[fila][columna] == 3 || mapa[fila][columna] == 4 || mapa[fila][columna] == 5 || mapa[fila][columna] == 6 || mapa[fila][columna] == 7 || mapa[fila][columna] == 8
-                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 15 || mapa[fila][columna] == 15) {
+                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 14 || mapa[fila][columna] == 15) {
                 posxMuro = (columna * 32) + 32;
                 posyMuro = fila * 32;
                 if (posxMuro == posxPersonaje && posyMuro == posyPersonaje) {
@@ -382,7 +371,7 @@ bool existeColisionDerecha(int posxPersonaje, int posyPersonaje) {
     {
         for (int columna = 0; columna < 20; columna++) {
             if (mapa[fila][columna] == 1 || mapa[fila][columna] == 2 || mapa[fila][columna] == 3 || mapa[fila][columna] == 4 || mapa[fila][columna] == 5 || mapa[fila][columna] == 6 || mapa[fila][columna] == 7 || mapa[fila][columna] == 8
-                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 15 || mapa[fila][columna] == 15) {
+                || mapa[fila][columna] == 9 || mapa[fila][columna] == 10 || mapa[fila][columna] == 11 || mapa[fila][columna] == 12 || mapa[fila][columna] == 13 || mapa[fila][columna] == 14 || mapa[fila][columna] == 15) {
                 posxMuro = (columna * 32) - 32;
                 posyMuro = fila * 32;
                 if (posxMuro == posxPersonaje && posyMuro == posyPersonaje) {
@@ -396,23 +385,162 @@ bool existeColisionDerecha(int posxPersonaje, int posyPersonaje) {
 
 void inicializar() {
     quit = false;
-    posx = 0;
-    posy = 0;
-
+    posx = 608;
+    posy = 608;
+    posxE = 96;
+    posyE = 416;
+    vidas = 3;
 
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+
+    /*Comando para reproducir la musica, solo necesita una muscia de fondo*/
+    //Mix_PlayingMusic(musica, -1);
     estadoteclado = SDL_GetKeyboardState(NULL);
 
     window = SDL_CreateWindow("SDL2 Moving Wizard", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, 0);
     renderer = SDL_CreateRenderer(window, -1, 0);
     image = IMG_Load("arquera.png");
-    texture = SDL_CreateTextureFromSurface(renderer, image);
+    texturapersonaje = SDL_CreateTextureFromSurface(renderer, image);
+
+    enemigo = IMG_Load("slime.png");
+    texturaenemigo = SDL_CreateTextureFromSurface(renderer, enemigo);
 
     fondo = IMG_Load("fondos.png");
     texturafondo = SDL_CreateTextureFromSurface(renderer, fondo);
     SDL_FreeSurface(fondo);
     SDL_SetRenderDrawColor(renderer, 168, 230, 255, 255);
+
+    efectocofre = Mix_LoadWAV("Cofre.wav");
+}
+
+void leerEvento() {
+    while (SDL_PollEvent(&event) != NULL)
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_UP) {
+                if (posy <= 0 || existeColisionArriba(posx, posy)) {
+                    break;
+                }
+                else {
+                    posy -= 32;
+                }
+            }
+            if (event.key.keysym.sym == SDLK_DOWN) {
+                if (posy > 576 || existeColisionAbajo(posx, posy)) {
+                    break;
+                }
+                else {
+                    posy += 32;
+                }
+            }
+            if (event.key.keysym.sym == SDLK_LEFT) {
+                if (posx <= 0 || existeColisionIzquierda(posx, posy)) {
+                    break;
+                }
+                else {
+                    posx -= 32;
+                }
+            }
+            if (event.key.keysym.sym == SDLK_RIGHT) {
+                if (posx > 576 || existeColisionDerecha(posx, posy)) {
+                    break;
+                }
+                else {
+                    posx += 32;
+                }
+            }
+            break;
+        }
+    }
+}
+
+void pantallaDerrota() {
+    bool salir = false;
+    ventanaDerrota = SDL_CreateWindow("Derrota", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, 0);
+    renderer = SDL_CreateRenderer(ventanaDerrota, -1, 0);
+    while (!salir) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        
+        while (SDL_PollEvent(&event) != NULL)
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                salir = true;
+                break;
+            }
+        }
+        SDL_RenderPresent(renderer);
+        SDL_RenderClear(renderer);
+    }
+    SDL_DestroyWindow(ventanaDerrota);
+    
+}
+
+void escribirVidas() {
+    rectFuente = { 160, 3904, 32, 32 };//CAMBIAR POR CORAZON...ESTO ES UN EJEMPLO
+
+    if (vidas >= 3) {
+        rectPanel = { 748,64,32,32 };
+        SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
+    }
+    if (vidas >= 2) {
+        rectPanel = { 700,64,32,32 };
+        SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
+        }
+    if (vidas >= 1) {
+        rectPanel = { 652,64,32,32 };
+        SDL_RenderCopy(renderer, texturafondo, &rectFuente, &rectPanel);
+    }
+    else {
+        SDL_DestroyWindow(window);
+        pantallaDerrota();
+    }    
+    
+}
+
+void existecolisionenemigo() {
+    if (posx==posxE && posy == posyE) {
+        vidas -= 1;
+        posx = 608;
+        posy = 608;
+    }
+}
+
+void tiempoYSprites() {
+    Uint32 ticks = SDL_GetTicks();
+    Uint32 sprite = (ticks / 100) % 5;
+    SDL_Rect srcrect = { sprite * 32, 0, 32, 32 };
+    SDL_Rect dstrect = { posx, posy, 32, 32 };
+    SDL_Rect rectenemigo = { posxE, posyE, 32, 32 };
+
+    SDL_RenderClear(renderer);
+    pintarmapa(dstrect);
+    panel();
+    escribirVidas();
+    existecolisionenemigo();
+    SDL_RenderCopy(renderer, texturapersonaje, &srcrect, &dstrect);
+    SDL_RenderCopy(renderer, texturaenemigo, &srcrect, &rectenemigo);
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 168, 230, 255, 255);
+    SDL_RenderClear(renderer);
+}
+
+void finalizar() {
+    SDL_DestroyTexture(texturapersonaje);
+    SDL_FreeSurface(image);
+    Mix_FreeChunk(efectocofre);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
 }
 
 int main(int argc, char** argv)
@@ -421,71 +549,10 @@ int main(int argc, char** argv)
     inicializar();
     while (!quit)
     {
-        Uint32 ticks = SDL_GetTicks();
-        Uint32 sprite = (ticks / 100) % 5;
-        SDL_Rect srcrect = { sprite * 32, 0, 32, 32 };
-        SDL_Rect dstrect = { posx, posy, 32, 32 };
-
-        
-        while (SDL_PollEvent(&event) != NULL)
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_UP) {
-                    if (posy <= 0 || existeColisionArriba(posx, posy)) {
-                        break;
-                    }
-                    else {
-                        posy -= 32;
-                    }
-                }
-                if (event.key.keysym.sym == SDLK_DOWN) {
-                    if (posy > 576 || existeColisionAbajo(posx, posy)) {
-                        break;
-                    }
-                    else {
-                        posy += 32;
-                    }
-                }
-                if (event.key.keysym.sym == SDLK_LEFT) {
-                    if (posx <= 0 || existeColisionIzquierda(posx, posy)) {
-                        break;
-                    }
-                    else {
-                        posx -= 32;
-                    }
-                }
-                if (event.key.keysym.sym == SDLK_RIGHT) {
-                    if (posx > 576 || existeColisionDerecha(posx, posy)) {
-                        break;
-                    }
-                    else {
-                        posx += 32;
-                    }
-                }
-                break;
-            }
-        }
-
-        SDL_RenderClear(renderer);
-        pintarmapa(dstrect);
-        panel();
-        SDL_RenderCopy(renderer, texture, &srcrect, &dstrect);
-        SDL_RenderPresent(renderer);
-        SDL_SetRenderDrawColor(renderer, 168, 230, 255, 255);
-        SDL_RenderClear(renderer);
-
+        tiempoYSprites();
+        leerEvento();
     }
-
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(image);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
+    finalizar();
+    
     return 0;
 }
